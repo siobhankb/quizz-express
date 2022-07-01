ebe171a4be39951a69e4ad2662f122c2d7a7b95a
siobhankb <siobhan.boylan@gmail.com>
2022-07-01 11:32:21 -0500

    create question view

---
+++ src/routes/quiz/index.js
@@ -0,0 +1,6 @@
+const QuizDashboardRouter = require('express').Router();
+
+// QuizDashboardRouter.route('/quiz').get(require('/.editor.js')).post(require('./create.js'))
+
+
+module.exports = QuizDashboardRouter
